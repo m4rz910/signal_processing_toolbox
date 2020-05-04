fab29b917f1b6f12b3d2d1c576b852c83c2f2c15
m4rz910 <tzavelisz@gmail.com>
2020-05-04 00:07:26 -0400

    part 2 done

--- final_part2.py
+++ final_part2.py
@@ -70,41 +70,41 @@ for r_lab, color, line in zip(['R1','R2','R3','R4'],
     
 
 #     # ************ C80 **************
-    #time window first
-    peaks, _ = find_peaks(h,height=5)
-    # fig = plt.figure(); plt.xlim([0,0.05]);
-    # plt.plot(h); plt.plot(h.iloc[peaks], "x")
-    h_windowed = h.copy()
-    h_windowed = h_windowed.iloc[peaks[0]-50:] #50 samples back from first peak seems to work fine
-    #plt.figure(); plt.plot(h_windowed); plt.xlim([0,0.5]);
+#     #time window first
+#     peaks, _ = find_peaks(h,height=5)
+#     # fig = plt.figure(); plt.xlim([0,0.05]);
+#     # plt.plot(h); plt.plot(h.iloc[peaks], "x")
+#     h_windowed = h.copy()
+#     h_windowed = h_windowed.iloc[peaks[0]-50:] #50 samples back from first peak seems to work fine
+#     #plt.figure(); plt.plot(h_windowed); plt.xlim([0,0.5]);
     
-    h_w_0_80 = h_windowed.copy(); h_w_80_inf = h_windowed.copy()
-    h_w_0_80.iloc[int(80/(source.dt*1000)):] = 0 #make everything after 80 ms 'zero'
-    h_w_80_inf.iloc[:int(80/(source.dt*1000))] =  0 #make everything before 80 ms 'zero'
+#     h_w_0_80 = h_windowed.copy(); h_w_80_inf = h_windowed.copy()
+#     h_w_0_80.iloc[int(80/(source.dt*1000)):] = 0 #make everything after 80 ms 'zero'
+#     h_w_80_inf.iloc[:int(80/(source.dt*1000))] =  0 #make everything before 80 ms 'zero'
     
-    # Octive Band Filter
-    cuts =  [125*2**i for i in range(0,6)]
-    octives = []
-    for i,dummy in enumerate(cuts):
-        #h_w_0_80
-        x = SignalTB.butter_bandpass_filter(data=h_w_0_80.values,
-                                            lowcut=cuts[i]/np.sqrt(2), highcut=cuts[i]*np.sqrt(2),
-                                            fs=fs, order=3)
-        h_w_0_80_filt = pd.Series(data=x, index=h_w_0_80.index)
+#     # Octive Band Filter
+#     cuts =  [125*2**i for i in range(0,6)]
+#     octives = []
+#     for i,dummy in enumerate(cuts):
+#         #h_w_0_80
+#         x = SignalTB.butter_bandpass_filter(data=h_w_0_80.values,
+#                                             lowcut=cuts[i]/np.sqrt(2), highcut=cuts[i]*np.sqrt(2),
+#                                             fs=fs, order=3)
+#         h_w_0_80_filt = pd.Series(data=x, index=h_w_0_80.index)
         
-        #h_w_80_inf
-        x = SignalTB.butter_bandpass_filter(data=h_w_80_inf.values,
-                                            lowcut=cuts[i]/np.sqrt(2), highcut=cuts[i]*np.sqrt(2),
-                                            fs=fs, order=3)
-        h_w_80_inf_filt = pd.Series(data=x, index=h_w_80_inf.index)
+#         #h_w_80_inf
+#         x = SignalTB.butter_bandpass_filter(data=h_w_80_inf.values,
+#                                             lowcut=cuts[i]/np.sqrt(2), highcut=cuts[i]*np.sqrt(2),
+#                                             fs=fs, order=3)
+#         h_w_80_inf_filt = pd.Series(data=x, index=h_w_80_inf.index)
         
-        h_filt_80 = h_w_0_80_filt.apply(lambda x: x**2).sum()
-        h_filt_inf = h_w_80_inf_filt.apply(lambda x: x**2).sum()
+#         h_filt_80 = h_w_0_80_filt.apply(lambda x: x**2).sum()
+#         h_filt_inf = h_w_80_inf_filt.apply(lambda x: x**2).sum()
         
-        octives.append((i, 10*np.log10(h_filt_80/h_filt_inf)))
-    plt.plot(*zip(*octives),line, c=color,marker='o',markersize=8, label=r_lab);
-plt.legend(); plt.grid(); plt.xlabel('Octive Band [Hz]'); plt.ylabel('C80');
-plt.xticks(range(0,6),cuts)
+#         octives.append((i, 10*np.log10(h_filt_80/h_filt_inf)))
+#     plt.plot(*zip(*octives),line, c=color,marker='o',markersize=8, label=r_lab);
+# plt.legend(); plt.grid(); plt.xlabel('Octave Band [Hz]'); plt.ylabel('C80');
+# plt.xticks(range(0,6),cuts)
 
 
 #     # ************ EDT **************  
@@ -127,36 +127,38 @@ plt.xticks(range(0,6),cuts)
         
 #         edc_filt = 10*np.log10(edc_filt/np.max(edc_filt))
 #         t_zero = edc_filt[round(edc_filt,1)==0].index[0]
-#         t_30 = edc_filt[round(edc_filt,1)==-10].index[0]
-#         octives.append((i, (t_30-t_zero)*6))
+#         t_end = edc_filt[round(edc_filt,1)==-10].index[0]
+#         octives.append((i, (t_end-t_zero)*6))
 #     plt.plot(*zip(*octives),line, c=color,marker='o',markersize=8, label=r_lab);
-# plt.legend(); plt.grid(); plt.xlabel('Octive Band [Hz]'); plt.ylabel('EDT');
+# plt.legend(); plt.grid(); plt.xlabel('Octave Band [Hz]'); plt.ylabel('EDT [s]');
 # plt.xticks(range(0,6),cuts)
 
 #     # ************ T30 ************** 
-#     h.loc[0.5:] = 0.001  #setting everything after 0.5 seconds 'zero'
-# # #    Octive Band Filter
-#     cuts =  [125*2**i for i in range(0,6)]
-#     octives = []
-#     for i,dummy in enumerate(cuts):
-#         x = SignalTB.butter_bandpass_filter(data=h.values,
-#                                             lowcut=cuts[i]/np.sqrt(2), highcut=cuts[i]*np.sqrt(2),
-#                                             fs=fs, order=3)        
-#         h_filt = pd.Series(data=x, index=h.index)
+    h.loc[0.5:] = 0.001  #setting everything after 0.5 seconds 'zero'
+# #    Octive Band Filter
+    cuts =  [125*2**i for i in range(0,6)]
+    octives = []
+    for i,dummy in enumerate(cuts):
+        x = SignalTB.butter_bandpass_filter(data=h.values,
+                                            lowcut=cuts[i]/np.sqrt(2), highcut=cuts[i]*np.sqrt(2),
+                                            fs=fs, order=3)        
+        h_filt = pd.Series(data=x, index=h.index)
         
-#         edc_filt = np.flip(np.cumsum(np.flip(h_filt.values)**2 *receiver.dt)) # flip, cumulative sum and flip back
-#         edc_filt = pd.Series(data=edc_filt, index=h_filt.index) #schroeder
+        edc_filt = np.flip(np.cumsum(np.flip(h_filt.values)**2 *receiver.dt)) # flip, cumulative sum and flip back
+        edc_filt = pd.Series(data=edc_filt, index=h_filt.index) #schroeder
         
-#         # plt.figure()
-#         # plt.plot(20*np.log10(h_filt/np.max(h_filt)));
-#         # plt.plot(10*np.log10(edc_filt/np.max(edc_filt)));
+        # plt.figure()
+        # plt.plot(20*np.log10(h_filt/np.max(h_filt)));
+        # plt.plot(10*np.log10(edc_filt/np.max(edc_filt)));
         
-#         edc_filt = 10*np.log10(edc_filt/np.max(edc_filt))
-#         t_zero = edc_filt[round(edc_filt,1)==-5].index[0] #changed from edt
-#         t_30 = edc_filt[round(edc_filt,1)==-35].index[0] #changed from edt
-#         octives.append((i, (t_30-t_zero)*2)) #changed from edt
-#     plt.plot(*zip(*octives),line, c=color,marker='o',markersize=8, label=r_lab);
-# plt.legend(); plt.grid(); plt.xlabel('Octive Band [Hz]'); plt.ylabel('T30');
-# plt.xticks(range(0,6),cuts)
+        edc_filt = 10*np.log10(edc_filt/np.max(edc_filt))
+        t_zero = edc_filt[round(edc_filt,1)==-5].index[0] #changed from edt
+        t_end = edc_filt[round(edc_filt,1)==-35].index[0] #changed from edt
+        octives.append((i, (t_end-t_zero)*2)) #changed from edt
+    plt.plot(*zip(*octives),line, c=color,marker='o',markersize=8, label=r_lab);
+plt.legend(); plt.grid(); plt.xlabel('Octave Band [Hz]'); plt.ylabel('T30 [s]');
+plt.xticks(range(0,6),cuts)
+
+fig.savefig('./plots/final_T30.png', dpi=300, bbox_inches='tight')
 
 
